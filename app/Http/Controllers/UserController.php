41b9d5497a2a45c0ee06c9d7dd0d7bf5c1e61d68
LuizAuGFer <luiz.augusto9922@outlook.com>
2023-11-13 18:54:23 -0300

    feat: new route for user registration

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    public function register(Request $request) {
+       
+        $user = \DB::table('users')->where('email', '=', $request->email)->first();
+
+        if($user == null) {
+           
+            try {
+                $user = new User;
+                $user->name = $request->name;
+                $user->email = $request->email;
+                $user->country = $request->country;
+                $user->password = hash('sha256', $request->password);
+                $user->save();
+
+            }catch(\Exception $e) {
+                return response()->json(['message' => 'Ocorreu um erro no cadastro do usuário!'])->setStatusCode(422);
+            }
+
+            return response()->json(['message' => 'Usuário cadastrado com sucesso!'])->setStatusCode(200);
+
+        }else {
+            return response()->json(['message' => 'O email informado não pode ser usado!'])->setStatusCode(422);
+        }
+    }
+
+}
